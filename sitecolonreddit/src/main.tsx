81eef56bb35799f3fb245bfd525bd94f59c11328
Chris Brown <cbrown4652@gmail.com>
2024-10-14 14:04:12 -0500

    Update for react 18

--- sitecolonreddit/src/main.tsx
+++ sitecolonreddit/src/main.tsx
@@ -1,12 +1,15 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@mui/material/styles';
 import App from './containers/App';
 import theme from './theme/theme';
 import './assets/global.css'
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>,
-  document.getElementById('root')
 );
